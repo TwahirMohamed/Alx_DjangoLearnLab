aca31c16fff31e8491c765bb6ed0173016eda6ab
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-17 21:01:24 -0500

    Checks for “Retrieve the librarian for a library.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -30,6 +30,7 @@ def list_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)
         print(f"The librarian for {library_name} is {library.librarian.name}")
     except Library.DoesNotExist:
         print(f"No library found with name: {library_name}")
