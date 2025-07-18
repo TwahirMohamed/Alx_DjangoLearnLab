430ae95746464db933b540823387fc84acb3a3ee
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-17 20:11:22 -0500

    file deletions starting over

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book  # This imports the model from models.py
-
-admin.site.register(Book) 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')   # Columns in list view
-    list_filter = ('publication_year',)                      # Sidebar filters
-    search_fields = ('title', 'author')   
--- django-models/LibraryProject/bookshelf/retrieve.md
+++
@@ -1,6 +0,0 @@
-# Retrieve the book created
-book = Book.objects.get(title="1984")
-print(book.title, book.author, book.publication_year)
-
-# Expected Output:
-# 1984 George Orwell 1949
