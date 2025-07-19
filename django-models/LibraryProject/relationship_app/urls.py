185b0115b1d911e70700088f7c48d969683d52c1
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-18 21:09:43 -0500

    fixed the error

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import book_list, LibraryDetailView, user_login, user_logout, register
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.book_list, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
     path('register/', views.register, name='register'),
 ]
