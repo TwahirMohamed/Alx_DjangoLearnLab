0caef110b85437b23ee3cfef06c3c6b32383f3aa
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-19 20:07:45 -0500

    Extend the User Model (UserProfile Model)

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 # Syntax
 # from django.db import models
@@ -10,6 +12,26 @@ from django.db import models
 # class Product(models.Model):
 #     name = models.CharField(max_length=100)
 #     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+
+# Signal to automatically create UserProfile on user creation
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
 class Author(models.Model):
     """Table for the author"""
     name = models.CharField(max_length=80)
