2fa6ef04c810e26a6d3411b28762b775ae8e15fd
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-19 20:41:30 -0500

    configure urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import book_list, LibraryDetailView, user_login, user_logout, register, admin_view, librarian_view, member_view
+from .views import book_list, LibraryDetailView, user_login, user_logout, register, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -9,6 +9,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
     path('register/', views.register, name='register'),
+    path('book/add/', add_book, name='add_book'),
+    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
 
     # Roles url
     path('admin-page/', admin_view, name='admin_view'),
