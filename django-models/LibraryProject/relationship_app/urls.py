caea0b6b05c71e77bb3bfcbb9a69486edfd1d255
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-19 21:03:34 -0500

    checks for updated urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,4 +17,9 @@ urlpatterns = [
     path('admin-page/', admin_view, name='admin_view'),
     path('librarian-page/', librarian_view, name='librarian_view'),
     path('member-page/', member_view, name='member_view'),
+
+    # Book operations with permissions
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
