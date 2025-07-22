54c6788c1611aff60b19e25bafba23fce66ecddb
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-22 17:35:37 -0500

    error correction

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.conf import settings
 from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
@@ -19,7 +20,7 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
