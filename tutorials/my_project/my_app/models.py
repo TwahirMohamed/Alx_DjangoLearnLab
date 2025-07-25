8cddecb783062db4f32f9694045c1eaec2830b7c
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 07:14:23 -0500

    updated book model

--- tutorials/my_project/my_app/models.py
+++ tutorials/my_project/my_app/models.py
@@ -5,4 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
