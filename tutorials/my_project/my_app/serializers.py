15658a9a585241b6336d8601eea9c1063d804f98
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 07:17:58 -0500

    extended the book serializer to include date created

--- tutorials/my_project/my_app/serializers.py
+++ tutorials/my_project/my_app/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    days_since_created = serializers.SerializerMethodField()
     class Meta:
         model = Book
         fields = '__all__'
+    def get_days_since_created(self, obj):
+        delta = datetime.now().date() - obj.created_at.date()
+        return delta.days
