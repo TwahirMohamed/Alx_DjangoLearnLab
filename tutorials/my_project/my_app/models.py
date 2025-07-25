20b51c529619aa6c6d8e7ef0a1cc988c28b13c84
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 07:20:51 -0500

    added time feature to the model

--- tutorials/my_project/my_app/models.py
+++ tutorials/my_project/my_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
 
     def __str__(self):
         return self.title
