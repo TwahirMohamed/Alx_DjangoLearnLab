fa6f56a9f12eb75b1d91b8c87f3ea771af719895
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-29 20:49:11 -0500

    dealt with urls for main project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
+    def get_queryset(self):
+        queryset = self.queryset
+        name_filter = self.request.query_params.get('name', None)
+        if name_filter is not None:
+            queryset = queryset.filter(name__icontains=name_filter)
+        return queryset
