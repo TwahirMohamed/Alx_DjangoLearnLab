81639964e67179f2d8e2bb5486e48055554c3c11
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-29 20:51:59 -0500

    Checks for “In api/views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
