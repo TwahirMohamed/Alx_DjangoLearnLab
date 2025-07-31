2f68f239468d95c92db093c4233fc7a1a9d1d73a
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-31 16:22:21 -0500

    step1:creating viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,9 +9,13 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
     
 
-    def get_queryset(self):
-        queryset = self.queryset
-        name_filter = self.request.query_params.get('name', None)
-        if name_filter is not None:
-            queryset = queryset.filter(name__icontains=name_filter)
-        return queryset
+    # def get_queryset(self):
+    #     queryset = self.queryset
+    #     name_filter = self.request.query_params.get('name', None)
+    #     if name_filter is not None:
+    #         queryset = queryset.filter(name__icontains=name_filter)
+    #     return queryset
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
