a97f9bda737d3b0dbb2b380e92ca1c2d3ec78791
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-31 16:50:51 -0500

    added import statement

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
