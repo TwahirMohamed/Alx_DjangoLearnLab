fced573e5916780e2e6ce8d391344b06c9edbc63
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-05 08:11:32 -0500

    created a custom serializer for the user model

--- tutorials/my_project/my_app/serializers.py
+++ tutorials/my_project/my_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, User
 from datetime import datetime
 
 
@@ -11,3 +11,18 @@ class BookSerializer(serializers.ModelSerializer):
     def get_days_since_created(self, obj):# customizing serializers
         delta = datetime.now().date() - obj.created_at.date()
         return delta.days
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    A custom serializer for a User model that includes full_name field calculated from the first_name and last_name fields in the model.
+    """
+    days_since_created = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'full_name']
+
+        def get_full_name(self, obj):
+            return f"The full name: {obj.first_name} {obj.last_name}".strip()
+        
