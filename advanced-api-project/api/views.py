6bacd78ab1ab9538999ea5a6c270c3af9f04d957
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-08 16:53:14 -0500

    implemented permissions for each view class

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, mixins, permissions
 from .models import Book, Author 
 from .serializers import BookSerializer, AuthorSerializer
 
@@ -8,16 +8,19 @@ class CustomBookListView(generics.ListAPIView):
 # to retrieve all books
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Public access
 # Retrieve single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Public access
 
 # Adding a new book 
 class BookCreateView(generics.CreateAPIView):
     model = Book
     serializer_class = BookSerializer
     fields = ['title', 'publication_year', 'author']
+    permission_classes = [permissions.IsAuthenticated]
 
     # function to perform book creatiion
     def perform_create(self, serializer):
@@ -28,7 +31,9 @@ class BookCreateView(generics.CreateAPIView):
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class  BookDeleteView(generics.DeleteAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
