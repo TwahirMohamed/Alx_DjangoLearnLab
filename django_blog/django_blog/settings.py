010d75397786c20d36710ae7989d29a89314a91c
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-11 16:54:52 -0500

    added database config

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'myuser',         # database username
+        'PASSWORD': 'mypassword', # database password
+        'HOST': 'localhost',      # or 'db' if using Docker
+        'PORT': '5432',
     }
 }
 
