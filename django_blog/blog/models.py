3effb289bf305b676c9d1de5e424051fadd0849e
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-14 19:57:02 -0500

    created two new models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,3 +17,22 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
+    
