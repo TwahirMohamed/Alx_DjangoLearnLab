568e7fb166dbd12df80a17ebce61bbca96aba352
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-22 20:09:36 -0500

    updated serializers as per the checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,26 +1,26 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
-class UserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField()
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+        model = get_user_model()
+        fields = ['username', 'email', 'bio', 'profile_picture', 'followers', 'following']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
-        Token.objects.create(user=user)  # automatically create a token
+        user = get_user_model().objects.create_user(**validated_data)
         return user
+    
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(validated_data)
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+        return user, token
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ('key',)
